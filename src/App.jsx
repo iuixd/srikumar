e33c328d86b57b055d0785764e9a1b22d4f1f2bc
Srikumar K <text2sri@gmail.com>
2025-06-22 19:55:20 +0530

    updated project links

--- src/App.jsx
+++ src/App.jsx
@@ -82,11 +82,11 @@ function App() {
 
                 <div className="group flex flex-row p-8 rounded-lg overflow-hidden w-full text-turquoise-900 transition-colors duration-500 ease-in-out hover:bg-turquoise-300 cursor-default">
                     <div className="mr-8 relative flex flex-col items-center justify-center">
-                      <div className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer">
+                      <a href="https://www.youtube.com/watch?v=cMCYiHNBtq4" className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer" target="_blank">
                         <svg className="ml-1 h-12 w-12 fill-turquoise-900" viewBox="0 0 16 16">
                           <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
                         </svg>
-                      </div>
+                      </a>
                       <img src={thumbnailLumenAI} className="w-4xl rounded-lg" alt="Thumbnail of Lumen AI Solutions" />
                     </div>
                     <div className="w-350 opacity-90 group-hover:opacity-100 transition">
@@ -118,11 +118,11 @@ function App() {
 
                 <div className="group flex flex-row p-8 rounded-lg overflow-hidden w-full text-turquoise-900 transition-colors duration-500 ease-in-out hover:bg-turquoise-300 cursor-default">
                     <div className="mr-8 relative flex flex-col items-center justify-center">
-                      <div className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer">
+                      <a href="https://www.youtube.com/watch?v=UwCw4N5_6SI" className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer" target="_blank">
                         <svg className="ml-1 h-12 w-12 fill-turquoise-900" viewBox="0 0 16 16">
                           <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
                         </svg>
-                      </div>
+                      </a>
                       <img src={thumbnailDataPrivacy} className="w-4xl rounded-lg" alt="Thumbnail of Data security & privacy automation" />
                     </div>
                     <div className="w-350 opacity-90 group-hover:opacity-100 transition">
@@ -137,11 +137,11 @@ function App() {
 
                 <div className="group flex flex-row p-8 rounded-lg overflow-hidden w-full text-turquoise-900 transition-colors duration-500 ease-in-out hover:bg-turquoise-300 cursor-default">
                     <div className="mr-8 relative flex flex-col items-center justify-center">
-                      <div className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer">
+                      <a href="https://www.youtube.com/watch?v=uW1a5iQ7u1Q&t=1s" className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer" target="_blank">
                         <svg className="ml-1 h-12 w-12 fill-turquoise-900" viewBox="0 0 16 16">
                           <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
                         </svg>
-                      </div>
+                      </a>
                       <img src={thumbnailZeroTrust} className="w-4xl rounded-lg" alt="Thumbnail of Data security & privacy automation" />
                     </div>
                     <div className="w-350 opacity-90 group-hover:opacity-100 transition">
@@ -176,11 +176,11 @@ function App() {
 
                 <div className="group flex flex-row p-8 rounded-lg overflow-hidden w-full text-turquoise-900 transition-colors duration-500 ease-in-out hover:bg-turquoise-300 cursor-default">
                     <div className="mr-8 relative flex flex-col items-center justify-center">
-                      <div className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer">
+                      <a href="https://www.youtube.com/watch?v=rYPZIdsFbAc&t=1s" className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer" target="_blank">
                         <svg className="ml-1 h-12 w-12 fill-turquoise-900" viewBox="0 0 16 16">
                           <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
                         </svg>
-                      </div>
+                      </a>
                       <img src={thumbnailPetAdoption} className="w-4xl rounded-lg" alt="Thumbnail of Data security & privacy automation" />
                     </div>
                     <div className="w-350 opacity-90 group-hover:opacity-100 transition">
