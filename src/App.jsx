ec1057a0012cc919a0919b61a3d0c8bed3ab6f75
Srikumar K <text2sri@gmail.com>
2025-06-22 20:00:39 +0530

    updated dashboard thumbnail

--- src/App.jsx
+++ src/App.jsx
@@ -156,11 +156,6 @@ function App() {
 
                 <div className="group flex flex-row p-8 rounded-lg overflow-hidden w-full text-turquoise-900 transition-colors duration-500 ease-in-out hover:bg-turquoise-300 cursor-default">
                     <div className="mr-8 relative flex flex-col items-center justify-center">
-                      <div className="absolute w-16 h-16 rounded-full bg-turquoise-300 opacity-50 hover:opacity-100 transition flex flex-col items-center justify-center cursor-pointer">
-                        <svg className="ml-1 h-12 w-12 fill-turquoise-900" viewBox="0 0 16 16">
-                          <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
-                        </svg>
-                      </div>
                       <img src={thumbnailUnifiedDashboard} className="w-4xl rounded-lg" alt="Thumbnail of Data security & privacy automation" />
                     </div>
                     <div className="w-350 opacity-90 group-hover:opacity-100 transition">
