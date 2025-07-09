2d76ec8564ce3e0412ed31a8b6061be09b8181f9
Srikumar K <text2sri@gmail.com>
2025-07-09 15:28:58 +0530

    check mobile to not to load video element

--- src/components/VideoBg.jsx
+++ src/components/VideoBg.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import videoBg from '../assets/videoBg.mp4';
+import useIsMobile from "./useIsMobile";
 
 const VideoBg = () => {
+    const isMobile = useIsMobile();
   return (
+    
     <>
-      <div className="block sm:block min-[360px]:hidden justify-center self-auto items-center w-full min-h-screen mt-0 flex absolute bg-[linear-gradient(to_bottom,_rgba(0,_192,_199,_0.5),_rgba(0,_192,_199,_0.5))]">
+      {isMobile ? (
+        <div className=" justify-center self-auto items-center w-full min-h-screen mt-0 flex absolute bg-[linear-gradient(to_bottom,_rgba(0,_192,_199,_0.5),_rgba(0,_192,_199,_0.5))]">
+      </div>
+      ) : (
+        <div className="justify-center self-auto items-center w-full min-h-screen mt-0 flex absolute bg-[linear-gradient(to_bottom,_rgba(0,_192,_199,_0.5),_rgba(0,_192,_199,_0.5))]">
         <video
           src={videoBg}
           autoPlay
           loop
           muted
           className="
-            block sm:block min-[360px]:hidden -z-10 object-cover bg-[50%] bg-cover w-full h-full m-auto absolute"
+            -z-10 object-cover bg-[50%] bg-cover w-full h-full m-auto absolute"
         ></video>
       </div>
+      )}
+      
       <div
         className="fixed h-[74px] z-2 top-0 self-auto w-full bg-turquoise-500 opacity-0"
         x-bind:class="{'bg-turquoise-500 opacity-0': !scrolledFromTop, 'bg-turquoise-500 opacity-50': scrolledFromTop}"
