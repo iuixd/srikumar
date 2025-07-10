4e1dd179444010cecfee2962e059adc4a6736ecb
Srikumar K <text2sri@gmail.com>
2025-07-10 10:55:17 +0530

    Fixed VideoBG bug

--- src/components/VideoBg.jsx
+++ src/components/VideoBg.jsx
@@ -3,33 +3,26 @@ import videoBg from '../assets/videoBg.mp4';
 import useShouldHideVideo from "./useShouldHideVideo";
 
 const VideoBg = () => {
-    const shouldHideVideo = useShouldHideVideo();
+    //const shouldHideVideo = useShouldHideVideo();
   return (
     
     <>
-      {shouldHideVideo ? (
-        <div className=" justify-center self-auto items-center w-full min-h-screen mt-0 flex absolute">
-      </div>
-      ) : (
-        <div className="justify-center self-auto items-center w-full min-h-screen mt-0 flex absolute bg-[linear-gradient(to_bottom,_rgba(0,_192,_199,_0.5),_rgba(0,_192,_199,_0.5))]">
+      <div className="justify-center self-auto items-center w-full min-h-screen mt-0 flex absolute bg-[linear-gradient(to_bottom,_rgba(0,_192,_199,_0.5),_rgba(0,_192,_199,_0.5))]">
         <video
           src={videoBg}
           autoPlay
           loop
           muted
           className="
-            -z-10 object-cover bg-[50%] bg-cover w-full h-full m-auto absolute"
+            min-[360px]:invisible sm:visible -z-10 object-cover bg-[50%] bg-cover w-full h-full m-auto absolute"
         ></video>
       </div>
-      )}
-      
       <div
         className="fixed h-[74px] z-2 top-0 self-auto w-full bg-turquoise-500 opacity-0"
         x-bind:class="{'bg-turquoise-500 opacity-0': !scrolledFromTop, 'bg-turquoise-500 opacity-50': scrolledFromTop}"
         x-transiton
       ></div>
     </>
-  );
-};
+  )};
 
 export default VideoBg;
